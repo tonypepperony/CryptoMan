2b98f8fc8e0ed4bb930a83f135830a07e5d056bb
Ruslan <rus.official@mail.ru>
2017-08-23 20:23:10 +0300

    Added ItemActivity, item_xml

---
+++ app/src/main/java/ru/tonyappl/cryptoman/activities/ItemActivity.java
@@ -0,0 +1,15 @@
+package ru.tonyappl.cryptoman.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.tonyappl.cryptoman.R;
+
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+}
